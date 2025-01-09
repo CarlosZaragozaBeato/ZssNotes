0f11feeb546e891b29b05e00c98a8657ccab246e
carlos zaragoza <czaragoza@vps-20b027ea.vps.ovh.net>
2025-01-09 17:46:18 +0000

    s

--- ZssNotes/settings.py
+++ ZssNotes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = '/home/czaragoza/ZssNotes/static/'
-MEDIA_ROOT = '/home/czaragoza/ZssNotes/media/'
-
+# settings.py
 STATIC_URL = '/static/'
+
+# Directory to collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
